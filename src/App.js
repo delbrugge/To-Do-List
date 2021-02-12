2163ff90fadc4fec0a2194eadc8fde04612e4c70
delbrugge <delbrugge@gmail.com>
2021-02-12 12:05:31 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      < TodoList/>
+    </div>
+    
+  );
+}
+
+export default App;
